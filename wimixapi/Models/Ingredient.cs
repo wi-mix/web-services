ae812d2324cf085ef87ea234d557d5feb3e086a3
Ryan Huard <ryan.huard@live.ca>
2018-01-30 15:29:22 -0700

    Add models and stub out controller

---
+++ wimixapi/Models/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wimixapi.Models
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public short Amount { get; set; }
+
+        public int IngredientId { get; set; }
+    }
+}
---
+++ wimixapi/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wimixapi.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Ingredient> Ingredients { get; set; }
+        public int RecipeId { get; set; }
+
+    }
+}
